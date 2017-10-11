7355f55bf50472ab9467f8d39d3d56d749ccdab3
Til Blechschmidt <til@blechschmidt.de>
2017-10-11 23:09:41 +0200

    Added express server to serve the webpack output

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '../../frontend')));
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../../frontend/index.html'));
+});
+
+app.listen(8010, function () {
+    console.log('Example app listening on port 8080!')
+});
+
 import {populateRoutes, tickRoutes} from "./routes";
 import './websocket';
 
